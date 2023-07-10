59fe64618d03c4393ba40f3a1fc63651ae3603b3
Ha-no <gksdn1216@naver.com>
2023-07-10 17:46:45 +0900

    step 2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,52 +4,44 @@ import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const router = useRouter();
-
-  console.log('Pathname ');
-  console.log(router.pathname);
-
   return (
-    <nav className={styles.nav}>
-      <style jsx>
-        {`
-          .test {
-            color: green;
-          }
-        `}
-      </style>
-      <style jsx global>
-        {`
-          a {
-            text-decoration: none;
-          }
-          p {
-            background-color: lightblue;
-          }
-        `}
-      </style>
-      <Link href="/">
-        <p
-          className={`${styles.backgrey} ${
-            router.pathname === '/' ? styles.active : styles.noneactive
-          }`}
-        >
-          Home
-        </p>
-      </Link>
-      <Link href="/about">
-        <p
-          className={[
-            styles.backgrey,
-            router.pathname === '/about' ? styles.active : styles.noneactive,
-          ].join(' ')}
-        >
-          About
-        </p>
-      </Link>
-
-      <Link href="/category">
-        <p className="test">Category</p>
-      </Link>
+    <nav>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
